0a252e4ac36e005b4d526b964348608bfa91680c
David Way <david.way12@gmail.com>
2012-04-04 22:58:03 -0400

    first commit

---
+++ SchnapIt/SchnapItTests/SchnapItTests.h
@@ -0,0 +1,13 @@
+//
+//  SchnapItTests.h
+//  SchnapItTests
+//
+//  Created by David Way on 4/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SchnapItTests : SenTestCase
+
+@end
---
+++ SchnapIt/SchnapItTests/SchnapItTests.m
@@ -0,0 +1,32 @@
+//
+//  SchnapItTests.m
+//  SchnapItTests
+//
+//  Created by David Way on 4/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "SchnapItTests.h"
+
+@implementation SchnapItTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SchnapItTests");
+}
+
+@end
