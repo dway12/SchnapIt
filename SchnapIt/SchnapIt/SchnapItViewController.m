c5b6d4a0bb28cace531c47efe0fc6896492e267d
David Way <david.way12@gmail.com>
2012-04-10 15:10:05 -0400

    works on simulator now

--- SchnapIt/SchnapIt/SchnapItViewController.m
+++ SchnapIt/SchnapIt/SchnapItViewController.m
@@ -33,26 +33,26 @@
 {
     self.tagViewController = [[[TagViewController alloc] init] autorelease];
     
-    [self setCaptureManager:[[[CaptureSessionManager alloc] init] autorelease]];
-    
-	[[self captureManager] addVideoInput];
-    
-	[[self captureManager] addVideoPreviewLayer];
-	CGRect layerRect = CGRectMake(0, 0, 320, 375);
-	[[[self captureManager] previewLayer] setBounds:layerRect];
-	[[[self captureManager] previewLayer] setPosition:CGPointMake(CGRectGetMidX(layerRect),
-                                                                  CGRectGetMidY(layerRect))];
-	[[[self view] layer] addSublayer:[[self captureManager] previewLayer]];
+//    [self setCaptureManager:[[[CaptureSessionManager alloc] init] autorelease]];
+//    
+//	[[self captureManager] addVideoInput];
+//    
+//	[[self captureManager] addVideoPreviewLayer];
+//	CGRect layerRect = CGRectMake(0, 0, 320, 375);
+//	[[[self captureManager] previewLayer] setBounds:layerRect];
+//	[[[self captureManager] previewLayer] setPosition:CGPointMake(CGRectGetMidX(layerRect),
+//                                                                  CGRectGetMidY(layerRect))];
+//	[[[self view] layer] addSublayer:[[self captureManager] previewLayer]];
     [[self view] addSubview:self.toolbar];
     
-    [self.captureManager captureSession].sessionPreset = AVCaptureSessionPresetPhoto;
-    
-    self.stillImageOutput = [[AVCaptureStillImageOutput alloc] init];
-    NSDictionary *outputSettings = [[NSDictionary alloc] initWithObjectsAndKeys: AVVideoCodecJPEG, AVVideoCodecKey, nil];
-    [self.stillImageOutput setOutputSettings:outputSettings];
-    
-    [[self.captureManager captureSession] addOutput:self.stillImageOutput];
-    [[self.captureManager captureSession] startRunning];
+//    [self.captureManager captureSession].sessionPreset = AVCaptureSessionPresetPhoto;
+//    
+//    self.stillImageOutput = [[AVCaptureStillImageOutput alloc] init];
+//    NSDictionary *outputSettings = [[NSDictionary alloc] initWithObjectsAndKeys: AVVideoCodecJPEG, AVVideoCodecKey, nil];
+//    [self.stillImageOutput setOutputSettings:outputSettings];
+//    
+//    [[self.captureManager captureSession] addOutput:self.stillImageOutput];
+//    [[self.captureManager captureSession] startRunning];
     // Do any additional setup after loading the view from its nib.
 }
 
@@ -93,41 +93,43 @@
 
 -(IBAction)takePicture:(id)sender
 {
-    AVCaptureConnection *videoConnection = nil;
-	for (AVCaptureConnection *connection in self.stillImageOutput.connections)
-	{
-		for (AVCaptureInputPort *port in [connection inputPorts])
-		{
-			if ([[port mediaType] isEqual:AVMediaTypeVideo] )
-			{
-				videoConnection = connection;
-				break;
-			}
-		}
-		if (videoConnection) { break; }
-	}
-	NSLog(@"about to request a capture from: %@", self.stillImageOutput);
+//    AVCaptureConnection *videoConnection = nil;
+//	for (AVCaptureConnection *connection in self.stillImageOutput.connections)
+//	{
+//		for (AVCaptureInputPort *port in [connection inputPorts])
+//		{
+//			if ([[port mediaType] isEqual:AVMediaTypeVideo] )
+//			{
+//				videoConnection = connection;
+//				break;
+//			}
+//		}
+//		if (videoConnection) { break; }
+//	}
+//	NSLog(@"about to request a capture from: %@", self.stillImageOutput);
+//    
+//    
+//	[self.stillImageOutput captureStillImageAsynchronouslyFromConnection:videoConnection completionHandler: ^(CMSampleBufferRef imageSampleBuffer, NSError *error)
+//     {
+//         
+//         
+//         
+//         NSData *imageData = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageSampleBuffer];
+//         self.picture = [[UIImage alloc] initWithData:imageData];
+//         
+//         [self.tagViewController setImage:self.picture];
+//         
+//         [self.navigationController pushViewController:self.tagViewController animated:YES];
+//         
+//         NSLog(@"%@",self.picture);
+//         
+//         //set picture, push controller...
+//         // self.vImage.image = image;
+//	 }];
     
-    
-	[self.stillImageOutput captureStillImageAsynchronouslyFromConnection:videoConnection completionHandler: ^(CMSampleBufferRef imageSampleBuffer, NSError *error)
-     {
-         
-         
-         
-         NSData *imageData = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageSampleBuffer];
-         self.picture = [[UIImage alloc] initWithData:imageData];
-         
-         [self.tagViewController setImage:self.picture];
-         
-         [self.navigationController pushViewController:self.tagViewController animated:YES];
-         
-         NSLog(@"%@",self.picture);
-         
-         //set picture, push controller...
-         // self.vImage.image = image;
-	 }];
-    
-    
+    self.picture = [UIImage imageNamed:@"MIT.jpg"];
+    [self.tagViewController setImage:self.picture];
+    [self.navigationController pushViewController:self.tagViewController animated:YES];
     
 }
 
